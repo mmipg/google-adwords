5333f228e33a6095a2df562538a459dff9fb53cb
ckevinhill <hill.ck@pg.com>
2015-01-05 15:27:15 -0500

    201409 reference updates

--- Google Adwords Nodes/src/com/pg/google/api/adwords/query/node/GoogleAdwordsQueryDialog.java
+++ Google Adwords Nodes/src/com/pg/google/api/adwords/query/node/GoogleAdwordsQueryDialog.java
@@ -2,90 +2,113 @@ package com.pg.google.api.adwords.query.node;
 
 
 import java.awt.Desktop;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.Properties;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.SpinnerDateModel;
 
 import org.knime.core.node.InvalidSettingsException;
-import org.knime.core.node.NodeDialogPane;
 import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.NotConfigurableException;
 import org.knime.core.node.port.PortObjectSpec;
 
-import com.google.api.ads.adwords.lib.jaxb.v201402.ReportDefinitionReportType;
+import com.google.api.ads.adwords.lib.jaxb.v201409.ReportDefinitionReportType;
 import com.google.common.base.Joiner;
 import com.pg.api.adwords.connector.data.GoogleAdwordsConnection;
 import com.pg.api.adwords.connector.data.GoogleAdwordsConnectionPortObjectSpec;
 import com.pg.knime.node.SortedComboBoxModel;
+import com.pg.knime.node.StandardNodeDialogPane;
 
-public class GoogleAdwordsQueryDialog extends NodeDialogPane {
+public class GoogleAdwordsQueryDialog extends StandardNodeDialogPane {
 
 	private static final NodeLogger LOGGER = NodeLogger.getLogger(GoogleAdwordsQueryDialog.class);
 	
 	private GoogleAdwordsConnection connection;
 	private HashMap<String, String[]> reportFields;
 	
-	private SortedComboBoxModel<String> cbxReportTypes;
-	private JTextArea txtMetrics;
-	private JSpinner spnStartDate, spnEndDate;
+	private SortedComboBoxModel<String> cbmReportTypes = new SortedComboBoxModel<>();
+	private JTextArea txtMetrics = new JTextArea();
+	private JLabel lblReportDescription = new JLabel();
+	private JSpinner spnStartDate = new JSpinner(new SpinnerDateModel());
+	private JSpinner spnEndDate = new JSpinner(new SpinnerDateModel());
 	
 	private static final String DATE_FORMAT = "yyyy-MM-dd";
 	private static final SimpleDateFormat SDF = new SimpleDateFormat(DATE_FORMAT);
+	private static final Properties reportDescriptions = new Properties();
+	
     
     public GoogleAdwordsQueryDialog() {
 
-    	int ypos = 0;
-    	
-    	JPanel pnlSettings = new JPanel(new GridBagLayout());
-    	pnlSettings.add( new JLabel("Report Type:"), getGBC(0,ypos++,0,0));
-    	cbxReportTypes = new SortedComboBoxModel<>();
-    	pnlSettings.add(new JComboBox<String>(cbxReportTypes), getGBC(0,ypos++,0,0));
-    	
+    	// Setup Help Icon
     	// https://developers.google.com/adwords/api/docs/appendix/reports
-    	pnlSettings.add( new JLabel("Report Metrics (comma-seperate, case-sensitive):"), getGBC(0,ypos++,0,0));
-    	txtMetrics = new JTextArea();
-    	txtMetrics.setRows(5);
+    	URL imgURL = getClass().getResource("support-icon.png");
+        ImageIcon icon = new ImageIcon(imgURL,"Report Description");
+        lblReportDescription = new JLabel("", icon, JLabel.LEFT);
+        lblReportDescription.setToolTipText("Description of selected report");
+        
+    	// Report Description Listener Setup
+        JComboBox<String> cbxReportTypes = new JComboBox<String>(cbmReportTypes);
+        cbxReportTypes.addActionListener(new GetReportDescriptionActionListener());
+        
+        // Metric area
+    	txtMetrics.setText("\n\n\n\n\n\n\n");
     	txtMetrics.setWrapStyleWord(true);
-    	pnlSettings.add(txtMetrics,getGBC(0,ypos++,0,0) );
     	
-    	JButton btnMetrics = new JButton("Load report metrics");
+    	
+    	// Load Metrics Button
+    	JButton btnMetrics = new JButton("Get All Fields");
     	btnMetrics.addActionListener(new ReportTypeListListener());
-    	pnlSettings.add(btnMetrics,getGBC(0,ypos++,0,0) );
     	
-    	JButton btnLink = new JButton("Click to see report details");
+    	// Help Button
+    	JButton btnLink = new JButton("Report Details");
     	btnLink.addActionListener(new MetricLinkListener());
-    	pnlSettings.add(btnLink,getGBC(0,ypos++,0,0) );
-
-    	pnlSettings.add( new JLabel("Start Date:"), getGBC(0,ypos++,0,0));
-    	spnStartDate = new JSpinner(new SpinnerDateModel());
-    	spnStartDate.setEditor(new JSpinner.DateEditor(spnStartDate, DATE_FORMAT));
-    	pnlSettings.add( spnStartDate, getGBC(0,ypos++,0,0));
     	
-    	pnlSettings.add( new JLabel("End Date:"), getGBC(0,ypos++,0,0));
-    	spnEndDate = new JSpinner(new SpinnerDateModel());
+    	// Spinner Setups
+    	spnStartDate.setEditor(new JSpinner.DateEditor(spnStartDate, DATE_FORMAT));
     	spnEndDate.setEditor(new JSpinner.DateEditor(spnEndDate, DATE_FORMAT));
     	
-    	pnlSettings.add( spnEndDate, getGBC(0,ypos++,0,0));
+    	addTab(
+    		"Settings",
+			buildStandardPanel(
+				new PanelBuilder()
+					.add("Report Type", cbxReportTypes, btnLink)
+					.add( null, lblReportDescription )
+					.add(new LabelComponentPair("Report Metrics", new JScrollPane(txtMetrics), btnMetrics, true))
+					.add( "Start Date", spnStartDate )
+					.add( "End Date", spnEndDate )
+					.build()
+			)
+		);
+ 
+    	for ( ReportDefinitionReportType type :  ReportDefinitionReportType.values() ) {
+    		if ( type != null && !"UNKNOWN".equals(type.name()) )
+    			cbmReportTypes.addElement(type.name());
+    	}
     	
-    	pnlSettings.add( new JPanel(), getGBC(0,ypos++,0,100));
-    	addTab("Settings", pnlSettings);
+        // Load Table Description Property File
+        try {
+        	reportDescriptions.load(getClass().getResourceAsStream("reportdescription.properties"));
+        } catch (IOException exc ) {
+        	LOGGER.warn("Unable to open tabledescription.properties file: " + exc.getMessage());
+        }
+    	
     }
 
     /**
@@ -96,7 +119,7 @@ public class GoogleAdwordsQueryDialog extends NodeDialogPane {
  
     	GoogleAdwordsQueryConfiguration config = new GoogleAdwordsQueryConfiguration();
     	
-    	config.setReportTypeName((String)cbxReportTypes.getSelectedItem());
+    	config.setReportTypeName((String)cbmReportTypes.getSelectedItem());
     	
     	String metrics = txtMetrics.getText();
     	metrics = metrics.replaceAll("\\s","");
@@ -124,45 +147,47 @@ public class GoogleAdwordsQueryDialog extends NodeDialogPane {
     		connection = spec.getGoogleAdwordsConnection();	
     	}
     	
-    	for ( ReportDefinitionReportType type :  ReportDefinitionReportType.values() ) {
-    		if ( type != null && !"UNKNOWN".equals(type.name()) )
-    			cbxReportTypes.addElement(type.name());
-    	}
+
     	
     	GoogleAdwordsQueryConfiguration config = new GoogleAdwordsQueryConfiguration();
     	config.load(settings);
     	
-    	txtMetrics.setText(Joiner.on(", \n").join(config.getReportMetrics()));
-    	cbxReportTypes.setSelectedItem(config.getReportTypeName());
+    	if ( config.getReportMetrics() != null )
+    		txtMetrics.setText(Joiner.on(", \n").join(config.getReportMetrics()));
+    	
+    	if ( config.getReportTypeName() != null )
+    		cbmReportTypes.setSelectedItem(config.getReportTypeName());
+    	
     	
     	try {
-    		spnStartDate.setValue(SDF.parse(config.getStartDate()));
-    		spnEndDate.setValue(SDF.parse(config.getEndDate()));
-    	} catch ( ParseException pexc ) {
+    		if ( config.getStartDate() != null )
+    			spnStartDate.setValue(SDF.parse(config.getStartDate()));
+    		
+    		if ( config.getEndDate() != null )
+    			spnEndDate.setValue(SDF.parse(config.getEndDate()));
+    	} catch ( ParseException | NullPointerException pexc ) {
     		LOGGER.error("Unable to parse start or end date: " + pexc.getMessage());
     	}
-    }
-
-    private static GridBagConstraints getGBC( int gridx, int gridy, int weightx, int weighty ) {
-    	return new GridBagConstraints(
-				gridx, 							// gridx
-				gridy, 							// gridy
-				1, 								// gridwidth
-				1,								// gridheight
-				weightx,						// weightx
-				weighty, 						// weighty
-				GridBagConstraints.NORTHWEST, 	// anchor
-				GridBagConstraints.HORIZONTAL, 	// fill
-				new Insets(5, 5, 5, 5), 		// insets
-				0, 								// ipadx
-				0);								// ipady
     }
     
     class MetricLinkListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-    		String url = "https://developers.google.com/adwords/api/docs/appendix/reports";
+			
+			String url = "https://developers.google.com/adwords/api/docs/appendix/reports";
+			
+			String reportName = (String)cbmReportTypes.getSelectedItem();
+			if ( reportName != null ) {
+				reportName = reportName.toLowerCase();
+				reportName = reportName.replaceAll("_performance_report", "");
+				reportName = reportName.replaceAll("_feed_item_report", "");
+				reportName = reportName.replaceAll("_report", "");
+				reportName = reportName.replaceAll("_", "-").toLowerCase();
+				reportName = reportName.replaceAll("adgroup", "ad-group");
+				url += "#" + reportName;
+			}
+		    		
 			
 			if ( !Desktop.isDesktopSupported() ) {
 				LOGGER.debug("Desktop support is not enabled");
@@ -189,22 +214,47 @@ public class GoogleAdwordsQueryDialog extends NodeDialogPane {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			
-			String reportName = (String)cbxReportTypes.getSelectedItem();
+			String reportName = (String)cbmReportTypes.getSelectedItem();
 			String[] fieldNames = reportFields.get(reportName);
 			
+			// Guard Statement:
+			if ( connection == null ) {
+				LOGGER.error("Unable to get metrics without Adwords Connector configured");
+				return;
+			}
+			
 			// Load Report fields
-    		if ( connection != null && fieldNames == null ) {
+    		if ( connection != null && ( fieldNames == null || fieldNames.length == 0) ) {
     			
-    			com.google.api.ads.adwords.axis.v201402.cm.ReportDefinitionReportType rt = com.google.api.ads.adwords.axis.v201402.cm.ReportDefinitionReportType.fromString(reportName);
+    			com.google.api.ads.adwords.axis.v201409.cm.ReportDefinitionReportType rt = com.google.api.ads.adwords.axis.v201409.cm.ReportDefinitionReportType.fromString(reportName);
     			reportFields.put(reportName, connection.getReportFields(rt));
     			fieldNames = reportFields.get(reportName);
     		}
     		
     		String concatenatedFieldNames = Joiner.on(", \n").join(fieldNames);
     		txtMetrics.setText(concatenatedFieldNames);
+    		txtMetrics.setMaximumSize(getPanel().getSize());
+    		getPanel().revalidate();
 		}
-
 		
+    	
+    }
+    
+    class GetReportDescriptionActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			String selectedTable = (String)cbmReportTypes.getSelectedItem();
+			if ( selectedTable == null || selectedTable.isEmpty() ) return;
+			
+			String description = (String)reportDescriptions.get(selectedTable);
+			
+			if ( description == null ) 
+				description = "Unknown Report";
+			
+			lblReportDescription.setText("<html><p>" + description + "</p></html>");
+		}
     	
     }
     
